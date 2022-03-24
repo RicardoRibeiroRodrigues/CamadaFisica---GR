bf44881120af69d120f5853aa38ac4f908d2d0f5
guilherme fontana <guifl2001@hotmail.com>
2022-03-24 14:25:51 -0300

    aplicação com inicio de lógica carareto

--- projeto4/server/aplicacao_server.py
+++ projeto4/server/aplicacao_server.py
@@ -26,12 +26,22 @@ serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 # serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
 # serialName = "COM6"                  # Windows(variacao de)
 
-TIPO_1 = b"\x00" # (Handshake)
-TIPO_2 = b"\x01" # (Resposta handshake)
-TIPO_3 = b"\x02" # (Mensagem de envio de dados)
-TIPO_4 = b"\x04" # (Confirmação de recebimento)
-TIPO_5 = b"\x05" # (Mensagem de timeout)
-TIPO_6 = b"\x06" # (Mensagem de erro no recebimento de dados)
+# Tipos de pacote
+# Chamado do cliente para o servidor (HandShake)
+TIPO_1 = b"\x01"
+# Resposta do servidor para o cliente ao HandShake
+TIPO_2 = b"\x02"
+# Mensagem do tipo de dados.
+TIPO_3 = b"\x03"
+# Mensagem recebida pelo servidor
+TIPO_4 = b"\x04"
+# Mensagem de time out -> Dois lados
+TIPO_5 = b"\x05"
+# Mensagem de erro -> Servidor envia
+TIPO_6 = b"\x06"
+
+EOP = b"\xAA\xBB\xCC\xDD"
+
 DADOS = 2
 COMANDOS = b"\x03"
 CONFIRMACAO = b"\x04"
@@ -39,10 +49,9 @@ ERRO = b"\x05"
 IPV4 = b"\x01"
 IPV6 = b"\x02"
 ID_SERVER = 0
-EOP = b"\xFF\xFF\xFF\xFF"
 
 
-def resposta(com1, head, resposta, client, n_pacote=0):
+def resposta(com1, head, resposta, n_pacote=0):
 
     n_pacote = n_pacote.to_bytes(1, byteorder='big')
     header = monta_header(
@@ -50,7 +59,7 @@ def resposta(com1, head, resposta, client, n_pacote=0):
         IPV6,
         n_pacote,
         b"\x01",
-        client,
+        b"\x01",
         b"\x00",
         b"\x01",
         b"\x00\x00\x00",
@@ -77,47 +86,51 @@ def main():
             ocioso = True
             content = b""
             rxBufferHeader, nRx = com1.getData(10)
-            if rxBufferHeader[0] == TIPO_1 and rxBufferHeader[4] == ID_SERVER:
-                client = rxBufferHeader[5]
-                size = rxBufferHeader[3]
-                info, _ = com1.getData(size)
-                final, _ = com1.getData(4)
-                resposta(com1, rxBufferHeader, TIPO_2, client)
-            if rxBufferHeader[0] == TIPO_3 and rxBufferHeader[4] == ID_SERVER:
-                message_size = rxBufferHeader[6]
-                i = 0
-                while i < message_size:
-                    try:
-                        if i != 0:
-                            rxBufferHeader, nRx = com1.getData(10)
-                        client = rxBufferHeader[5]
-                        size = rxBufferHeader[3]
-                        info, _ = com1.getData(size)
-                        time.sleep(0.05)
-                        final, _ = com1.getData(4)
-                        pacote_certo = rxBufferHeader[2] == i
-                        if not pacote_certo:
-                            print("O número do pacote está incorreto, reenviando número do pacote correto!")
-                        print("info: {}" .format(info))
-                        print("i: {}".format(i))
-                        if bytes_to_list(final) == bytes_to_list(EOP) and pacote_certo:
-                            content += info
-                            i += 1
-                            resposta(com1, rxBufferHeader, TIPO_4, client)
-                            print("uma resposta recebida")
-                        else:
-                            print("deu errado, to no else")
-                            if com1.rx.getBufferLen() > 0:
-                                print("Tamanho informado errado")
-                            com1.rx.clearBuffer()
-                            resposta(com1, rxBufferHeader, TIPO_6, client, i)
-                    except TimeoutError:
-                        print("Tempo excedido! Tentar novamente.")
-                        resposta(com1, rxBufferHeader, TIPO_5, client, i)
+            i = 0
+            while ocioso:
+                if rxBufferHeader[0] == TIPO_1:
+                    ocioso = False
+                elif rxBufferHeader[0] == TIPO_3:
+                    message_size = rxBufferHeader[6]
+                    ocioso = False
+            time.sleep(1)
+            client = rxBufferHeader[5]
+            size = rxBufferHeader[3]
+            info, _ = com1.getData(size)
+            final, _ = com1.getData(4)
+            if i == size:
                 with open("img/icon.png", "wb") as img:
                     img.write(content)
                 print("Receba!!!! Graças a deus, SIUUUUU!!!")
                 break
+            if rxBufferHeader[0] == TIPO_1:
+                resposta(com1, rxBufferHeader, TIPO_2)
+                ocioso = True
+            elif rxBufferHeader[0] == TIPO_3:
+                pacote_certo = rxBufferHeader[2] == i
+                if not pacote_certo:
+                    print("O número do pacote está incorreto, reenviando número do pacote correto!")
+                if bytes_to_list(final) == bytes_to_list(EOP) and pacote_certo:
+                    content += info
+                    i += 1
+                    resposta(com1, rxBufferHeader, TIPO_4)
+                    print("uma resposta recebida")
+                    ocioso = True
+                else:
+                    print("deu errado, to no else")
+                    if com1.rx.getBufferLen() > 0:
+                        print("Tamanho informado errado")
+                    com1.rx.clearBuffer()
+                    resposta(com1, rxBufferHeader, TIPO_6, i)
+                    ocioso = True
+    except TimeoutError:
+        print("Tempo excedido! Tentar novamente.")
+        resposta(com1, rxBufferHeader, TIPO_5, i)
+
+
+
+
+
                         
 
 
