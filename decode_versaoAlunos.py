d3ac68fea8f15e2494cbc0dfd15ff54990834fda
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-05-12 13:34:38 -0300

    Decode finalizado ?

--- decode_versaoAlunos.py
+++ decode_versaoAlunos.py
@@ -7,12 +7,51 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import time
 from suaBibSignal import *
+import peakutils
+import sys
+
+
+DTMF_MAP = {
+    1: (697, 1206),
+    2: (697, 1339),
+    3: (697, 1477),
+    "A": (697, 1633),
+    4: (770, 1206),
+    5: (770, 1339),
+    6: (770, 1477),
+    "B": (770, 1633),
+    7: (852, 1206),
+    8: (852, 1339),
+    9: (852, 1477),
+    "C": (852, 1633),
+    "X": (941, 1206),
+    0: (941, 1339),
+    "#": (941, 1477),
+    "D": (941, 1633),
+}
+
+
+def signal_handler(signal, frame):
+    print("You pressed Ctrl+C!")
+    sys.exit(0)
 
 
 # funcao para transformas intensidade acustica em dB
 def todB(s):
     sdB = 10 * np.log10(s)
     return sdB
+
+
+def caractere_mais_prox(frequencias):
+    mais_prox = None
+    menor_dist = (10, 10)
+    for char, tupla in DTMF_MAP.items():
+        diff_0 = abs(frequencias[0] - tupla[0]) < menor_dist[0]
+        diff_1 = abs(frequencias[1] - tupla[1]) < menor_dist[1]
+        if diff_0 and diff_1:
+            mais_prox = char
+            menor_dist = (diff_0, diff_1)
+    return mais_prox
 
 
 def main():
@@ -25,9 +64,8 @@ def main():
     fs = 44_100
     sd.default.samplerate = fs  # Taxa de amostragem
     sd.default.channels = 2  # Voce pode ter que alterar isso dependendo da sua placa
-    duration = (
-        1  # Tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
-    )
+    # Tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
+    duration = 2
     signal = signalMeu()
 
     # Faça um print na tela dizendo que a captacao comecará em n segundos. e entao
@@ -45,12 +83,8 @@ def main():
     print("...     FIM")
 
     # analise sua variavel "audio". pode ser um vetor com 1 ou 2 colunas, lista ...
-    # TODO: Continuar daqui
-    print(audio)
 
-    y = audio[0:]
-    print(y)
-
+    y = audio[:, 0]
     # grave uma variavel com apenas a parte que interessa (dados)
 
     # use a funcao linspace e crie o vetor tempo. Um instante correspondente a cada amostra!
@@ -60,11 +94,15 @@ def main():
     t = np.linspace(inicio, fim, numPontos)
 
     # plot do gravico  áudio vs tempo!
+    plt.plot(t, y)
+    plt.title("Audio no tempo")
+    plt.xlabel("Tempo em segundos")
+    plt.ylabel("Sinal")
 
     ## Calcula e exibe o Fourier do sinal audio. como saida tem-se a amplitude e as frequencias
     xf, yf = signal.calcFFT(y, fs)
     plt.figure("F(y)")
-    plt.plot(xf, yf)
+    plt.plot(xf, np.abs(yf))
     plt.grid()
     plt.title("Fourier audio")
 
@@ -73,11 +111,16 @@ def main():
     # voce deve tambem evitar que dois picos proximos sejam identificados, pois pequenas variacoes na
     # frequencia do sinal podem gerar mais de um pico, e na verdade tempos apenas 1.
 
-    # index = peakutils.indexes(,,)
-
+    index = peakutils.indexes(np.abs(yf), thres=0.1, min_dist=200)
+    freq_picos = xf[index]
     # printe os picos encontrados!
+    for freq in xf[index]:
+        print(f"Frequencia de pico: {freq}")
 
     # encontre na tabela duas frequencias proximas às frequencias de pico encontradas e descubra qual foi a tecla
+    tecla = caractere_mais_prox(freq_picos)
+    print(f"A tecla é a {tecla}")
+
     # print a tecla.
 
     ## Exibe gráficos
